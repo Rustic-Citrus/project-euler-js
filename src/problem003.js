b020b5f57ff4ee0b42cfd6394729886fe506cc93
Rustic-Citrus <mrcurtis.english@gmail.com>
2024-05-01 11:37:58 +0100

    refactor: made largestPrimeFactor into exported arrow function

--- src/problem003.js
+++ src/problem003.js
@@ -1,36 +1,34 @@
-function largestPrimeFactor(number) {
-    const factorsArr = []; // factors of number
-    let counter = 0;
-    let primeFactor = 0;
+export const largestPrimeFactor = (number) => {
+  const factorsArr = []; // factors of number
+  let counter = 0;
+  let primeFactor = 0;
 
-    // What are the factors of number?
-    while (counter <= number) {
-      if (number % counter == 0) {
-        factorsArr.push(counter);
-      }
-      counter++;
+  // What are the factors of number?
+  while (counter <= number) {
+    if (number % counter == 0) {
+      factorsArr.push(counter);
     }
-
-    // Which factors are prime numbers?
-    for (let i = 0; i < factorsArr.length; i++) {
-
-        // What are the factors of factorsArr[i]?
-        const nestedFactorsArr = [];
-        counter = 0;
-        while (counter <= factorsArr[i]) {
-            if (factorsArr[i] % counter == 0) {
-                nestedFactorsArr.push(counter);
-            }
-            counter++;
-        }
-
-        // Is factorsArr[i] a prime number?
-        if (nestedFactorsArr.length == 2) {
-            primeFactor = factorsArr[i];
-        }
-    }
-
-    return primeFactor;
+    counter++;
   }
-  
-console.log(largestPrimeFactor(14));
+
+  // Which factors are prime numbers?
+  for (let i = 0; i < factorsArr.length; i++) {
+
+      // What are the factors of factorsArr[i]?
+      const nestedFactorsArr = [];
+      counter = 0;
+      while (counter <= factorsArr[i]) {
+          if (factorsArr[i] % counter == 0) {
+              nestedFactorsArr.push(counter);
+          }
+          counter++;
+      }
+
+      // Is factorsArr[i] a prime number?
+      if (nestedFactorsArr.length == 2) {
+          primeFactor = factorsArr[i];
+      }
+  }
+
+  return primeFactor;
+}
