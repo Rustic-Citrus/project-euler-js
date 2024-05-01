7003e27cc0a157552be131b3722e4cbfc72330f6
Rustic-Citrus <mrcurtis.english@gmail.com>
2024-05-01 11:56:11 +0100

    test: added tests for problem003

--- spec/problem003.spec.js
+++ spec/problem003.spec.js
@@ -8,4 +8,25 @@ describe("Problem #1 Tests (Largest Prime Factor),", () => {
 
     expect(actualType).toBe(expectedType);
   });
+
+  it("should return the correct answer", () => {
+    const expectedAnswers = [ 2, 3, 5, 7, 2, 29 ];
+    const argumentsForActualAnswers = [ 2, 3, 5, 7, 8, 13195 ];
+
+    for (let i = 0; i < expectedAnswers.length; i++) {
+      const actualAnswer = largestPrimeFactor(argumentsForActualAnswers[i]);
+
+      expect(actualAnswer).toEqual(expectedAnswers[i]);
+    }
+  });
+
+  it("should return the correct answer even for large numbers", () => {
+    pending("massively slows down runtime");
+
+    const expected = 6857;
+
+    const actual = largestPrimeFactor(600851475143);
+
+    expect(actual).toEqual(expected);
+  });
 });
