986f468a33fd83cb2cbca3d8c1977f49ad589219
Rustic-Citrus <mrcurtis.english@gmail.com>
2024-05-01 11:57:14 +0100

    refactor: problem003

--- src/problem003.js
+++ src/problem003.js
@@ -12,23 +12,20 @@ export const largestPrimeFactor = (number) => {
   }
 
   // Which factors are prime numbers?
-  for (let i = 0; i < factorsArr.length; i++) {
+  for (const element of factorsArr) {
+    // What are the factors of factorsArr[i]?
+    const nestedFactorsArr = [];
+    counter = 0;
+    while (counter <= element) {
+      if (element % counter == 0) nestedFactorsArr.push(counter);
+      counter++;
+    }
 
-      // What are the factors of factorsArr[i]?
-      const nestedFactorsArr = [];
-      counter = 0;
-      while (counter <= factorsArr[i]) {
-          if (factorsArr[i] % counter == 0) {
-              nestedFactorsArr.push(counter);
-          }
-          counter++;
-      }
-
-      // Is factorsArr[i] a prime number?
-      if (nestedFactorsArr.length == 2) {
-          primeFactor = factorsArr[i];
-      }
+    // Is factorsArr[i] a prime number?
+    if (nestedFactorsArr.length == 2) {
+      primeFactor = element;
+    }
   }
 
   return primeFactor;
-}
+};
