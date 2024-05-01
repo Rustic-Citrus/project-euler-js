ec9be94595a6ead9f58f6c946bb376cd9da8ae26
Rustic-Citrus <mrcurtis.english@gmail.com>
2024-05-01 11:28:15 +0100

    feat: added problem002

--- src/problem002.js
+++ src/problem002.js
@@ -1,19 +1,18 @@
-function fiboEvenSum(n) {
-    let sum1 = 0;
-    const arr = [1, 2];
-    while (sum1 < n) {
-      sum1 = arr[arr.length-1] + arr[arr.length-2];
-      arr.push(sum1);
-    }
-
-    let sum2 = 0;
-    for (let i = 0; i < arr.length; i++) {
-      if (arr[i] % 2 == 0) {
-        sum2 += arr[i];
-      }
-    }
-
-    return sum2;
+export const fibonacciEvenSum = (n) => {
+  let sum1 = 0;
+  const arr = [1, 2];
+  while (sum1 < n) {
+    sum1 = arr[arr.length-1] + arr[arr.length-2];
+    arr.push(sum1);
   }
 
-console.log(fiboEvenSum(10))
+  let sum2 = 0;
+
+  for (const element of arr) {
+    if (element % 2 == 0) {
+      sum2 += element;
+    }
+  }
+
+  return sum2;
+};
